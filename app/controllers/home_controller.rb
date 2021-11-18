a45b5b47510d49c7f5813d13a18d3baa13f6d16a
ranjiayu <ranjiayu@outlook.com>
2021-11-18 12:44:38 +0800

    first version done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @version = Rails.application.config_for(:app).version
+    @materials = Rails.application.config_for(:app).dict
   end
 
   def about
--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -242,4 +242,18 @@ window.onload = () => {
     });
   }
 
+  // 导入资料库
+  let importBtn = document.querySelector("#import-materials");
+  if (importBtn) {
+    importBtn.addEventListener("click", () => {
+      let path = document.querySelector("#material-path").value;
+      // console.log(path);
+      fetch(path).then((res) => {
+        return res.text()
+      }).then((result) => {
+        // console.log(result);
+        document.querySelector("#words").innerHTML = result;
+      })
+    });
+  }
 };
